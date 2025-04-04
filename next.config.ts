569db285d5b5fee9f2f1a65b5920a95bc8c9e6c4
PTDTW <113230263+PTDTW@users.noreply.github.com>
2025-04-04 16:13:38 +0800

    feat: update assetPrefix in next.config.ts to remove trailing slash

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  basePath: '/day01',
-  assetPrefix: '/day01/', // 確保靜態資源也能正常加載
+  assetPrefix: '/day01', // 確保靜態資源也能正常加載
 };
 
 export default nextConfig;
