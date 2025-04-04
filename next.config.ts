a085425ecf8f784cb34461f39b799b930e9c66d0
PTDTW <113230263+PTDTW@users.noreply.github.com>
2025-04-04 16:07:51 +0800

    feat: update assetPrefix in next.config.ts to root path

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   basePath: '/day01',
-  assetPrefix: '/day01/', // 確保靜態資源也能正常加載
+  assetPrefix: '/', // 確保靜態資源也能正常加載
 };
 
 export default nextConfig;
